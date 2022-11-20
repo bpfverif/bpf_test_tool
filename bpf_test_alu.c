3f540618aec09b8c3df6f4ce5e7bb488a671010c
Harishankar Vishwanathan <harishankar.vishwanathan@rutgers.edu>
2022-11-20 00:28:11 -0500

    Add test for ALU and JMP

---
+++ bpf_test_alu.c
@@ -0,0 +1,166 @@
+/*
+  This program is used to test the output (r0) of bpf program. 
+  Replace `{183, 0, 0, 0, 1},...,{183, 9, 0, 0, 10},` with the bpf program. 
+  Then, run `make` and `./bpf_test` to get the output value.
+
+  The logic of this program: store the bpf program output, then put 
+  this value into the key-value map where the key is `0` (map[0] = output). 
+  Finally, from userspace, use key `0` to get the value (map[0]) from the 
+  key-value map and print it. Noting that this bpf program is attached 
+  to eth0 raw socket, so it is packet-driven, i.e., once there is a packet 
+  in eth0, this program will be run. Thus, a 3s-waiting time is set.
+*/
+
+#include <assert.h>
+#include <sys/socket.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <stddef.h>
+#include "libbpf.h"
+
+#define DEBUG
+
+#define MAP_STORE(r_i)                                                         \
+        BPF_LDX_MEM(BPF_DW, BPF_REG_9, BPF_REG_10, -(r_i + 1) * size),         \
+        BPF_MOV64_IMM(BPF_REG_0, r_i),                                         \
+        BPF_STX_MEM(BPF_W, BPF_REG_10, BPF_REG_0, -10 * size - 4),             \
+        BPF_MOV64_REG(BPF_REG_2, BPF_REG_10),                                  \
+        BPF_ALU64_IMM(BPF_ADD, BPF_REG_2, -10 * size -4),                      \
+        BPF_LD_MAP_FD(BPF_REG_1, map_fd),                                      \
+        BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem),   \
+        BPF_JMP_IMM(BPF_JEQ, BPF_REG_0, 0, 1),                                 \
+        BPF_STX_MEM(BPF_DW, BPF_REG_0, BPF_REG_9, 0),                          \
+
+static int test_bpf_prog_output(void)
+{
+	int sock = -1, map_fd, prog_fd, i, key;
+	long long value = 0, val[10];
+
+	map_fd = bpf_create_map(BPF_MAP_TYPE_ARRAY, sizeof(key), sizeof(value),
+				256);
+	if (map_fd < 0) {
+		printf("failed to create map '%s'\n", strerror(errno));
+		goto cleanup;
+	}
+
+	int size = 8;
+
+	struct bpf_insn prog[] = {
+
+#ifdef DEBUG
+		BPF_MOV64_IMM(BPF_REG_0, 0),
+		BPF_MOV64_IMM(BPF_REG_2, 0),
+		BPF_MOV64_IMM(BPF_REG_3, 0),
+		BPF_MOV64_IMM(BPF_REG_4, 0),
+		BPF_MOV64_IMM(BPF_REG_5, 0),
+		BPF_MOV64_IMM(BPF_REG_6, 0),
+		BPF_MOV64_IMM(BPF_REG_7, 0),
+		BPF_MOV64_IMM(BPF_REG_8, 0),
+		BPF_MOV64_IMM(BPF_REG_9, 0),
+
+		// test program start
+		{183, 0, 0, 0, 1},  // r0 = 1
+		{183, 1, 0, 0, 2},  // r1 = 2
+		{183, 2, 0, 0, 3},  // r2 = 3
+		{183, 3, 0, 0, 4},  // r3 = 4
+		{183, 4, 0, 0, 5},  // r4 = 5
+		{183, 5, 0, 0, 6},  // r5 = 6
+		{183, 6, 0, 0, 7},  // r6 = 7
+		{183, 7, 0, 0, 8},  // r7 = 8
+		{183, 8, 0, 0, 9},  // r8 = 9
+		{183, 9, 0, 0, 10}, // r9 = 10
+		// test program end
+#endif 
+
+// alu32
+#if 1
+		// r1=0xffffffffffffffff
+		BPF_LD_IMM64(BPF_REG_1, 0xffffffffffffffff), 
+		
+		// r2=0x0
+		BPF_LD_IMM64(BPF_REG_2, 0xa35bbc2f4bf4fdf2),
+
+		// r2=<unknown>
+	//	BPF_LD_IMM64(BPF_REG_2, 0xa35bbc2f4bf4fdf2),
+	//	BPF_ALU64_IMM(BPF_NEG, BPF_REG_2, 0),
+	//	BPF_ALU64_IMM(BPF_NEG, BPF_REG_2, 0),
+
+		// r1 = r1 & r2
+		BPF_ALU32_REG(BPF_AND, BPF_REG_1, BPF_REG_2),
+#endif
+
+#ifdef DEBUG
+		// store r0 - r9 in the stack
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_0, -1 * size), // *(u64 *)(fp - 8) = r0 
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_1, -2 * size),
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_2, -3 * size),
+	 	BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_3, -4 * size),
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_4, -5 * size),
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_5, -6 * size),
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_6, -7 * size),
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_7, -8 * size),
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_8, -9 * size),
+		BPF_STX_MEM(BPF_DW, BPF_REG_10, BPF_REG_9, -10 * size),
+
+		// reading all registers (including those containing memory values)
+		// requires cap_sys_admin
+		MAP_STORE(0)
+		MAP_STORE(1)
+		MAP_STORE(2)
+		MAP_STORE(3)
+		MAP_STORE(4)
+		MAP_STORE(5)
+		MAP_STORE(6)
+		MAP_STORE(7)
+		MAP_STORE(8)
+		MAP_STORE(9)
+#endif 	
+
+		// r0 is the return value
+		BPF_MOV64_IMM(BPF_REG_0, 0), /* r0 = 0 */
+		BPF_EXIT_INSN(),
+	};
+
+	prog_fd = bpf_prog_load(BPF_PROG_TYPE_SOCKET_FILTER, prog, sizeof(prog),
+				"GPL", 0);
+
+	// Attempt to print the bpf_log_buf for verifier information
+	printf("Log buffer:\n %s\n---\n", bpf_log_buf);
+
+	if (prog_fd < 0) {
+		printf("failed to load prog '%s'\n", strerror(errno));
+		goto cleanup;
+	}
+
+#ifdef DEBUG
+	sock = open_raw_sock("lo");
+
+	if (setsockopt(sock, SOL_SOCKET, SO_ATTACH_BPF, &prog_fd,
+		       sizeof(prog_fd)) < 0) {
+		printf("setsockopt %s\n", strerror(errno));
+		goto cleanup;
+	}
+
+	sleep(3);
+
+	for (int i = 0; i < 10; i++) {
+		key = i;
+		assert(bpf_lookup_elem(map_fd, &key, &val[0]) == 0);
+		printf("r%d: %llx %lld \n", i, val[0], val[0]);	
+	}
+#endif
+
+cleanup:
+	/* maps, programs, raw sockets will auto cleanup on process exit */
+	return 0;
+}
+
+int main(void)
+{
+	FILE *f;
+
+	f = popen("ping -c5 localhost", "r");
+	(void)f;
+
+	return test_bpf_prog_output();
+}
