33ec358ee0bd80a351a799487858c4f1e6963cad
Harishankar Vishwanathan <harishankar.vishwanathan@rutgers.edu>
2023-07-03 21:26:51 -0400

    update bpf_test.sh to print relevant verifier log lines, after update to bpf prog generator

--- bpf_test.sh
+++ bpf_test.sh
@@ -27,27 +27,31 @@ eval $exp
 # compile, and get output
 make clean > /dev/null
 sudo make bpf_test > /dev/null
-echo "-------"
 sudo ./bpf_test > bpf_test_output.txt
+echo "-----------------------------------------------------"
 
 # Extract the last two lines from bpf_prog_file
 last_line=$(tail -n 1 "$bpf_prog_file")
 IFS=';' read -ra parts <<< "$last_line" # Set the delimiter to ';' using IFS
 last_insn_line_num="${parts[0]/* /}"
-branch_to_print_line_num="${parts[1]/ \*\//}"
-branch_to_print_line_num=$((branch_to_print_line_num)) # convert to integer
-echo "-------"
+jump_outcome="${parts[1]/ \*\//}"
+jump_outcome=$((jump_outcome)) # convert to integer
 
-grep_exp_str="^$last_insn_line_num: ("
-grep_exp_str="\"$grep_exp_str\""
-grep_exp_branch="^$branch_to_print_line_num: " # only in case of a true branch
-if [ "$branch_to_print_line_num" -ge 0 ]; then
-  grep_exp="grep ${grep_exp_str} bpf_test_output.txt | tail -n 2; echo "..."; grep -A 1 ${grep_exp_branch} bpf_test_output.txt | tail -n 2"
+if [ "$jump_outcome" -eq 1 ]; then
+  grep_exp_str_1="\"^$last_insn_line_num: (\""
+  grep_exp_1="grep ${grep_exp_str_1} bpf_test_output.txt"
+  eval $grep_exp_1
+  echo "..."
+  true_branch_inst_num=$((last_insn_line_num + 3))
+  grep_exp_str_2="\"^$true_branch_inst_num: \""
+  grep_exp_2="grep -m 1 ${grep_exp_str_2} bpf_test_output.txt"
+  eval $grep_exp_2
 else
+  grep_exp_str="\"^$last_insn_line_num: (\""
   grep_exp="grep -A 1 ${grep_exp_str}  bpf_test_output.txt | tail -n 2"
+  eval $grep_exp
 fi
-eval $grep_exp
 
-echo "-------"
+echo "-----------------------------------------------------"
 grep "^r[0-9]: " bpf_test_output.txt
 
